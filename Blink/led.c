1b46738fc424441307c7c644423c8b70aa96ddbf
Nabil Islam <119033503+Nabilislam722@users.noreply.github.com>
2025-01-07 16:20:36 +0600

    Add files via upload
    Must Read the readme.md

---
+++ Blink/led.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void){
+   
+   
+    DDRB |= 1 << DDB5;
+
+    while (1)
+    {
+        PORTB |= 1 << PORTB5;
+
+          _delay_ms(1000);
+        
+        PORTB &= ~(1 << PORT5);
+
+         _delay_ms(1000);
+    }
+    
+}
